c45274ab70d302c2201359907d186c2237533b91
Ranjith <ranjithacharya102@gmail.com>
2023-12-16 11:07:10 +0530

    All the changes

--- P1/index.sh
+++ P1/index.sh
@@ -72,3 +72,12 @@ Select the Token
 
 Build Now
  Goto SonarQube Check the status
+
+
+ Docker
+sudo hostnamectl set-hostname docker
+/bin/bash
+sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl status docker
+sudo systemctl start docker
