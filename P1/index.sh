52969dd575f758fc85d753464e9ac14595c0c662
Ranjith <ranjithacharya102@gmail.com>
2023-12-16 11:03:01 +0530

    All the changes

--- P1/index.sh
+++ P1/index.sh
@@ -2,5 +2,7 @@ Jenkins CI/CD Pipeline - SonarQube, Docker, Github Webhooks on AWS
 
 https://www.youtube.com/watch?v=361bfIvXMBI
 
-
 Push the code into github
+
+Create a Three EC2 Machine
+Jenkins & Docker t2 Micro
