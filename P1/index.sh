83f7873b83a45a3f167040288dc14a8de1976a33
Ranjith <ranjithacharya102@gmail.com>
2023-12-16 11:08:37 +0530

    All the changes

--- P1/index.sh
+++ P1/index.sh
@@ -118,3 +118,17 @@ Dockerfile
 FROM nginx
 COPY . /usr/share/nginx/html/
 Save it
+
+Automated-Pipeline/configure
+Post Build Actions : Remove the remote shell | Execute Shell: scp ./* ubuntu @DIP:~/website/ 
+Save
+
+
+D-> Exit | ls | mkdir website | pwd | /home/ubuntu/website
+
+Build Now
+
+Automated-Pipeline/configure
+Post-Build Actions : Shell: scp -r ./* ubuntu @DIP:~/website/
+Build Now
+ Goto D -> ls
