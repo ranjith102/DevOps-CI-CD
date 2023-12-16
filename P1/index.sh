794cbe4809c16ab250964966c8341865c98ed0f8
Ranjith <ranjithacharya102@gmail.com>
2023-12-16 11:03:13 +0530

    All the changes

--- P1/index.sh
+++ P1/index.sh
@@ -6,3 +6,10 @@ Push the code into github
 
 Create a Three EC2 Machine
 Jenkins & Docker t2 Micro
+SonarQube t2 Medium
+Allow all the ports
+
+Jenkins
+sudo apt update -y
+sudo hostnamectl set-hostname jenkins
+/bin/bash
