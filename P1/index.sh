f03926388aaabe18573a17c31e8b2b47653ac214
Ranjith <ranjithacharya102@gmail.com>
2023-12-16 11:08:59 +0530

    All the changes

--- P1/index.sh
+++ P1/index.sh
@@ -132,3 +132,20 @@ Automated-Pipeline/configure
 Post-Build Actions : Shell: scp -r ./* ubuntu @DIP:~/website/
 Build Now
  Goto D -> ls
+
+
+ Automated-Pipeline/configure
+Post Build Actions: Add Build Steps: Remote Shell | shell cd /home/ubuntu/website
+
+D-> docker ps
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+docker build -t mywebsite .
+docker run -d -p 8085:80 --name=Onix-Website mywebsite
+Save
+Build Now
+
+D >
+Docker ps
+Copy the IP od Docker and see the result
