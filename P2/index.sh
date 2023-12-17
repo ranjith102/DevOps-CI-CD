285a23a35b02492aa5380456559f73aca6bd869e
Ranjith <ranjithacharya102@gmail.com>
2023-12-17 18:55:43 +0530

    Added

--- P2/index.sh
+++ P2/index.sh
@@ -1,3 +1,9 @@
 Jenkins CI/CD Pipeline - SonarQube, Docker, Github Webhooks on AWS
 
 https://www.youtube.com/watch?v=361bfIvXMBI
+
+Push the code into github
+
+Create a Three EC2 Machine
+Jenkins & Docker t2 Micro
+SonarQube t2 Medium
