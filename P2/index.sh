d751029958d17cb453eb226064d9bd832556dc15
Ranjith <ranjithacharya102@gmail.com>
2023-12-17 18:56:00 +0530

    Added

--- P2/index.sh
+++ P2/index.sh
@@ -7,3 +7,10 @@ Push the code into github
 Create a Three EC2 Machine
 Jenkins & Docker t2 Micro
 SonarQube t2 Medium
+Allow all the ports
+
+Jenkins
+sudo apt update -y
+sudo hostnamectl set-hostname jenkins
+/bin/bash
+sudo apt install openjdk-17-jre -y
